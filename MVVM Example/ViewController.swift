564a4db107214d3d8fafa9d4e867e455f393d430
Aydın ÜNAL <hands_up_@hotmail.co.uk>
2019-02-09 15:05:28 +0300

    Initial Commit

---
+++ MVVM Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM Example
+//
+//  Created by Aydın ÜNAL on 9.02.2019.
+//  Copyright © 2019 exmav10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MVVM ExampleTests/MVVM_ExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_ExampleTests.swift
+//  MVVM ExampleTests
+//
+//  Created by Aydın ÜNAL on 9.02.2019.
+//  Copyright © 2019 exmav10. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_Example
+
+class MVVM_ExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
